71e03ccf1f240b1b5a08555a8a27cfbaddbfedfe
momorito <jice.infimiforml@gmail.com>
2021-04-27 20:31:34 +0900

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="About">
+    <page-header class="mb-3">
+      {{ $t('当サイトについて') }}
+    </page-header>
+    <static-card>
+      {{
+        $t(
+          '当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために開設したものです。'
+        )
+      }}<br />
+      <br />
+      {{
+        $t(
+          '白井市の公式情報と客観的な数値をわかりやすく伝えることで、白井市民や関係者が、現状を把握して適切な対策を取れるようにすることを目的としています。'
+        )
+      }}
+    </static-card>
+    <static-card>
+      <h3>{{ $t('ブラウザ環境について') }}</h3>
+      <p>
+        {{ $t('当サイトは以下の環境でご覧いただくことを推奨いたします。') }}
+      </p>
+      <ul>
+        <li>{{ $t('Microsoft Edge 最新版') }}</li>
+        <li>{{ $t('Mozilla Firefox 最新版') }}</li>
+        <li>
+          {{ $t('Google Chrome 最新版（Windows 10以上, Android 8.0以上）') }}
+        </li>
+        <li>{{ $t('Safari 最新版（macOS, iOS）') }}</li>
+        <li>{{ $t('Opera 最新版') }}</li>
+      </ul>
+      <p class="StaticCard-Note">
+        <span>{{ $t('※') }}</span>
+        <span>
+          {{
+            $t(
+              '※ 推奨環境以外で利用された場合や、推奨環境下でもご利用のブラウザの設定等によっては、正しく表示されない場合がありますのでご了承ください。'
+            )
+          }}
+        </span>
+      </p>
+    </static-card>
+    <static-card>
+      <h3>{{ $t('当サイトへのリンクについて') }}</h3>
+      <p>{{ $t('当サイトへのリンクは自由です。') }}</p>
+    </static-card>
+    <static-card>
+      <h3>{{ $t('JavaScriptについて') }}</h3>
+      <p>
+        {{ $t('当サイトではJavaScriptを使用しております。') }}<br />
+        {{
+          $t(
+            'JavaScriptを無効にして使用された場合、各ページが正常に動作しない、または、表示されない場合がございます。'
+          )
+        }}<br />
+        {{
+          $t(
+            '当サイトをご利用の際には、JavaScriptを有効にして頂きますようお願いいたします。'
+          )
+        }}
+      </p>
+    </static-card>
+    <static-card>
+      <h3>{{ $t('クッキー (Cookie) について') }}</h3>
+      <p>
+        {{ $t('当サイトの一部ではクッキーを使用しています。') }}<br />
+        {{
+          $t(
+            'クッキーとは、Webコンテンツからの要求で利用者の手元の端末に一時的に保存されるデータのことで、当サイトでは利用状況の把握のためにクッキーを使用する場合があります。'
+          )
+        }}<br />
+      </p>
+      <p>
+        {{
+          $t(
+            'ブラウザに関する情報の収集を希望しない場合は、インターネット閲覧ソフト（ブラウザ）をご自身で設定することにより、クッキーの機能が働かないようにすることも可能です。'
+          )
+        }}
+      </p>
+      <p>
+        {{
+          $t(
+            'ただし、クッキーを受け入れない設定をされている場合は、当サイトの機能が正常に動作しない場合がございます。'
+          )
+        }}
+      </p>
+    </static-card>
+    
+    <static-card>
+      <h3>{{ $t('免責事項') }}</h3>
+      <p>
+        {{
+          $t(
+            '当サイトに掲載されている情報の正確性については万全を期していますが、東京都は利用者が当サイトの情報を用いて行う一切の行為について責任を負うものではありません。'
+          )
+        }}
+      </p>
+      <p>
+        {{
+          $t(
+            'また、利用者が当サイトを利用したことにより発生した利用者の損害及び利用者が第三者に与えた損害に対して、責任を負うものではありません。'
+          )
+        }}
+      </p>
+      <p>
+        {{
+          $t(
+            '当サイトに掲載されている情報は、予告なしに変更又は削除することがあります。'
+          )
+        }}
+      </p>
+    </static-card>
+    <static-card>
+      <h3>{{ $t('ソースコードについて') }}</h3>
+      <p>
+        {{
+          $t(
+            '当サイトのソースコードはMITライセンスで公開されており、誰でも自由に利用することができます。'
+          )
+        }}
+        <i18n path="詳しくは、{githubRepo}をご確認ください。">
+          <template #githubRepo>
+            <app-link
+              to="https://github.com/tokyo-metropolitan-gov/covid19"
+              :icon-size="16"
+            >
+              {{ $t('GitHub リポジトリ') }}
+            </app-link>
+          </template>
+        </i18n>
+      </p>
+    </static-card>
+    <static-card>
+      <h3>{{ $t('コンテンツについて') }}</h3>
+      <ul>
+        <li>
+          {{
+            $t(
+              'このサイトの内容物はクリエイティブ・コモンズ 表示 4.0 ライセンスの下に提供されています。'
+            )
+          }}
+        </li>
+        <li>
+          {{ $t('ただし商標等の他団体が権利を持つ以下のものは除きます。') }}
+          <ul>
+            <li>{{ $t('Gマーク（グッドデザイン賞受賞マーク）') }}</li>
+            <li>
+              {{
+                $t(
+                  '各SNS（LINE、Twitter、Facebook、GitHub、YouTube）ロゴマーク'
+                )
+              }}
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </static-card>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { MetaInfo } from 'vue-meta'
+
+import AppLink from '@/components/AppLink.vue'
+import PageHeader from '@/components/PageHeader.vue'
+import StaticCard from '@/components/StaticCard.vue'
+
+export default Vue.extend({
+  components: {
+    PageHeader,
+    StaticCard,
+    AppLink,
+  },
+  head(): MetaInfo {
+    return {
+      title: this.$t('当サイトについて') as string,
+    }
+  },
+})
+</script>
